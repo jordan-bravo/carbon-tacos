5bfd4e92802c735c5481757d59da760c42f31319
Jordan Brookman <Jordan.Brookman@ibm.com>
2021-09-29 13:29:56 -0400

    Add table skeleton loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import {
   Button,
   Content,
   DataTable,
+  DataTableSkeleton,
   Header,
   HeaderName,
   HeaderNavigation,
@@ -21,111 +22,66 @@ import {
 import { ingredientsHeaderData, ingredientsRowData } from "./ingredientsData";
 
 const App: React.FC = (): ReactElement => {
-  // const [calories, setCalories] = useState(0);
   const [totalFat, setTotalFat] = useState(0);
   const [totalProtein, setTotalProtein] = useState(0);
-  const [totalCarbs, setTotalCarbs] = useState(0);
+  const [totalCarb, setTotalCarb] = useState(0);
+  const [showTable, setShowTable] = useState(false);
 
-  const handleSelection = (selectedRows: any) => {
-    // let totalCalories = 0;
-    // let fatCalories = 0;
-    // let proteinCalories = 0;
-    // let carbCalories = 0;
-
-    for (var row of selectedRows) {
-      // totalCalories += row.cells[1].value;
-      setTotalFat(totalFat + row.cells[2].value);
-      setTotalProtein(totalProtein + row.cells[3].value);
-      setTotalCarbs(totalCarbs + row.cells[4].value);
+  const updateTotals: Function = (selectedRows: any): void => {
+    let totalFatValue = 0;
+    let totalProteinValue = 0;
+    let totalCarbValue = 0;
+    if (selectedRows.length !== 0) {
+      for (let row of selectedRows) {
+        totalFatValue += row.cells[2].value;
+        totalProteinValue += row.cells[3].value;
+        totalCarbValue += row.cells[4].value;
+      }
     }
-
-    // const totalMacros = fatCalories + proteinCalories + carbCalories;
-
-    // setCalories(totalCalories);
+    setTotalFat(totalFatValue);
+    setTotalProtein(totalProteinValue);
+    setTotalCarb(totalCarbValue);
   };
 
-  return (
-    <div>
-      <Header>
-        <HeaderName href="#">Tacos</HeaderName>
-        <HeaderNavigation aria-label="IBM [Platform]">
-          <HeaderMenuItem href="#">Menu</HeaderMenuItem>
-          <HeaderMenuItem href="#">Ingredients</HeaderMenuItem>
-          <HeaderMenuItem href="#">Locations</HeaderMenuItem>
-        </HeaderNavigation>
-      </Header>
-      <Content id="main-content">
-        <div className="bs--grid">
-          <div className="bx--row">
-            <div className="bx--col">
-              <h1 className="demo--heading">Build your own taco</h1>
-              <h2 className="demo--subheading">Nutritional totals</h2>
-            </div>
-          </div>
-          <div className="bx--row">
-            <div className="bx--col demo--col-bleed">
-              <Tile>
-                <h3 className="demo--label">Calories</h3>
-                <p className="demo--value">0</p>
-              </Tile>
-            </div>
-            <div className="bx--col demo--col-bleed">
-              <Tile>
-                <h3 className="demo--label">Total Fat</h3>
-                <p className="demo--value">{totalFat}g</p>
-              </Tile>
-            </div>
-            <div className="bx--col demo--col-bleed">
-              <Tile>
-                <h3 className="demo--label">Total Protein</h3>
-                <p className="demo--value">{totalProtein}g</p>
-              </Tile>
-            </div>
-            <div className="bx--col demo--col-bleed">
-              <Tile>
-                <h3 className="demo--label">Total Carbs</h3>
-                <p className="demo--value">{totalCarbs}g</p>
-              </Tile>
-            </div>
-          </div>
-          <div className="bx--row demo--row-cta">
-            <div className="bx--col">
-              <h2 className="demo--subheading">Choose your ingredients</h2>
-            </div>
-            <div className="bx--col demo--col-cta">
-              <Button>Add taco</Button>
-            </div>
-          </div>
-          <div className="bx--row demo--row-table">
-            <div className="bx--col demo--col-bleed"></div>
-          </div>
-        </div>
+  const returnDataTableSkeleton = (): ReactElement => {
+    return (
+      <DataTableSkeleton
+        columnCount={5}
+        rowCount={4}
+        compact={false}
+        zebra={false}
+        showHeader={false}
+        showToolbar={false}
+      />
+    );
+  };
 
-        <DataTable rows={ingredientsRowData} headers={ingredientsHeaderData}>
-          {({
-            rows,
-            headers,
-            getHeaderProps,
-            getRowProps,
-            getSelectionProps,
-            getTableProps,
-            getTableContainerProps,
-            selectedRows,
-          }: {
-            rows: any;
-            headers: any;
-            getHeaderProps: any;
-            getRowProps: any;
-            getSelectionProps: any;
-            getTableProps: any;
-            getTableContainerProps: any;
-            selectedRows: any;
-          }): ReactElement => (
+  const returnDataTable = (): ReactElement => {
+    return (
+      <DataTable rows={ingredientsRowData} headers={ingredientsHeaderData}>
+        {({
+          rows,
+          headers,
+          getHeaderProps,
+          getRowProps,
+          getSelectionProps,
+          getTableProps,
+          getTableContainerProps,
+          selectedRows,
+        }: {
+          rows: any;
+          headers: any;
+          getHeaderProps: any;
+          getRowProps: any;
+          getSelectionProps: any;
+          getTableProps: any;
+          getTableContainerProps: any;
+          selectedRows: any;
+        }): ReactElement => {
+          updateTotals(selectedRows);
+          return (
             <TableContainer {...getTableContainerProps()}>
-              <Table
-                onClick={() => handleSelection(selectedRows)}
-                {...getTableProps()}
-              >
+              <Table {...getTableProps()}>
                 <TableHead>
                   <TableRow>
                     <TableSelectAll {...getSelectionProps()} />
@@ -148,8 +104,70 @@ const App: React.FC = (): ReactElement => {
                 </TableBody>
               </Table>
             </TableContainer>
-          )}
-        </DataTable>
+          );
+        }}
+      </DataTable>
+    );
+  };
+
+  useEffect(() => {
+    setTimeout(async () => {
+      setShowTable(true);
+    }, 1500);
+  }, []);
+
+  return (
+    <div>
+      <Header>
+        <HeaderName href="#">Tacos</HeaderName>
+        <HeaderNavigation aria-label="IBM Tacos">
+          <HeaderMenuItem href="#">Menu</HeaderMenuItem>
+          <HeaderMenuItem href="#">Ingredients</HeaderMenuItem>
+          <HeaderMenuItem href="#">Locations</HeaderMenuItem>
+        </HeaderNavigation>
+      </Header>
+      <Content id="main-content">
+        <div className="bs--grid">
+          <div className="bx--row">
+            <div className="bx--col">
+              <h1 className="demo--heading">Build your own taco</h1>
+              <h2 className="demo--subheading">Nutritional totals</h2>
+            </div>
+          </div>
+          <div className="bx--row margin-horizontal-zero">
+            <div className="bx--col demo--col-bleed">
+              <Tile>
+                <h3 className="demo--label">Total Fat</h3>
+                <p className="demo--value">{totalFat}g</p>
+              </Tile>
+            </div>
+            <div className="bx--col demo--col-bleed">
+              <Tile>
+                <h3 className="demo--label">Total Protein</h3>
+                <p className="demo--value">{totalProtein}g</p>
+              </Tile>
+            </div>
+            <div className="bx--col demo--col-bleed">
+              <Tile>
+                <h3 className="demo--label">Total Carbs</h3>
+                <p className="demo--value">{totalCarb}g</p>
+              </Tile>
+            </div>
+          </div>
+          <div className="bx--row demo--row-cta margin-horizontal-zero">
+            <div className="bx--col">
+              <h2 className="demo--subheading">Choose your ingredients</h2>
+            </div>
+            <div className="bx--col demo--col-cta">
+              <Button>Add taco</Button>
+            </div>
+          </div>
+          <div className="bx--row demo--row-table">
+            <div className="bx--col demo--col-bleed"></div>
+          </div>
+        </div>
+        {!showTable && returnDataTableSkeleton()}
+        {showTable && returnDataTable()}
       </Content>
     </div>
   );
