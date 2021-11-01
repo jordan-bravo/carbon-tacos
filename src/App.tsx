fdd4f92b3534019e45afb2ef5624c3ab462c9073
Jordan Brookman <Jordan.Brookman@ibm.com>
2021-11-01 11:45:17 -0400

    Refactor to use Table Toolbar Search Carbon Component. Remove  explicit any types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { ReactElement, useEffect, useState } from "react";
 import {
-  Button,
   Content,
   Header,
   HeaderName,
   HeaderNavigation,
   HeaderMenuItem,
-  Search,
   Tile,
   InlineNotification,
   Grid,
@@ -19,6 +17,7 @@ import {
 } from "./data/ingredientsData";
 import DataTableSkeletonComponent from "./components/DataTableSkeletonComponent";
 import DataTableComponent from "./components/DataTableComponent";
+import { Rows } from "./models/dataTableModels";
 
 const App = (): ReactElement => {
   // State
@@ -27,7 +26,6 @@ const App = (): ReactElement => {
   const [totalCarb, setTotalCarb] = useState(0);
   const [showTable, setShowTable] = useState(false);
   const [showToastNotification, setShowToastNotification] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
 
   // Simulate a delayed response
   useEffect(() => {
@@ -37,34 +35,25 @@ const App = (): ReactElement => {
   }, []);
 
   // Methods
-  const addTacoHandler = () => {
+  const addTacoHandler = (): void => {
     setShowToastNotification(true);
     setTimeout(() => setShowToastNotification(false), 5000);
   };
 
-  const updateTotals: Function = (selectedRows: any): void => {
+  const updateTotals = (selectedRows: Array<Rows>): void => {
     let totalFatValue = 0;
     let totalProteinValue = 0;
     let totalCarbValue = 0;
     if (selectedRows.length !== 0) {
       for (let row of selectedRows) {
-        totalFatValue += row.cells[2].value;
-        totalProteinValue += row.cells[3].value;
-        totalCarbValue += row.cells[4].value;
+        totalFatValue += Number(row.cells[2].value);
+        totalProteinValue += Number(row.cells[3].value);
+        totalCarbValue += Number(row.cells[4].value);
       }
     }
     setTotalFat(totalFatValue);
     setTotalProtein(totalProteinValue);
     setTotalCarb(totalCarbValue);
-  };
-
-  const filterItems: Function = (array: any, query: string) => {
-    if (!query) return array;
-    else
-      return array.filter(
-        (element: any) =>
-          element.ingredient.toLowerCase().indexOf(query.toLowerCase()) !== -1
-      );
   };
 
   return (
@@ -116,26 +105,14 @@ const App = (): ReactElement => {
               </Tile>
             </Column>
           </Row>
-          <Row className="tacos--row-cta button-row">
-            <Column>
-              <h2 className="tacos--subheading">Choose your ingredients</h2>
-            </Column>
-            <Column className="tacos--col-cta">
-              <Button onClick={() => addTacoHandler()}>Add taco</Button>
-            </Column>
-          </Row>
-          <Search
-            size="lg"
-            placeholder="Search"
-            labelText="Search"
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          {!showTable && <DataTableSkeletonComponent />}
-          {showTable && (
+          {!showTable ? (
+            <DataTableSkeletonComponent />
+          ) : (
             <DataTableComponent
               updateTotals={updateTotals}
               headerData={ingredientsHeaderData}
-              rowData={filterItems(ingredientsRowData, searchTerm)}
+              rowData={ingredientsRowData}
+              addTacoHandler={addTacoHandler}
             />
           )}
         </Grid>
--- src/components/DataTableComponent.tsx
+++ src/components/DataTableComponent.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ChangeEventHandler, ReactElement } from "react";
 import {
   DataTable,
   TableContainer,
@@ -10,16 +10,24 @@ import {
   TableBody,
   TableSelectRow,
   TableCell,
+  TableToolbar,
+  TableToolbarContent,
+  TableToolbarSearch,
+  Button,
 } from "carbon-components-react";
+
+import { Rows, Headers, Cells, RowData } from "../models/dataTableModels";
 
 const DataTableComponent = ({
   updateTotals,
   headerData,
   rowData,
+  addTacoHandler,
 }: {
   updateTotals: Function;
-  headerData: any;
-  rowData: any;
+  headerData: Array<Headers>;
+  rowData: Array<RowData>;
+  addTacoHandler: Function;
 }): ReactElement => {
   return (
     <DataTable rows={rowData} headers={headerData} isSortable>
@@ -32,24 +40,40 @@ const DataTableComponent = ({
         getTableProps,
         getTableContainerProps,
         selectedRows,
+        onInputChange,
       }: {
-        rows: any;
-        headers: any;
-        getHeaderProps: any;
-        getRowProps: any;
-        getSelectionProps: any;
-        getTableProps: any;
-        getTableContainerProps: any;
-        selectedRows: any;
+        rows: Array<Rows>;
+        headers: Array<Headers>;
+        getHeaderProps: Function;
+        getRowProps: Function;
+        getSelectionProps: Function;
+        getTableProps: Function;
+        getTableContainerProps: Function;
+        selectedRows: Array<Rows>;
+        onInputChange: ChangeEventHandler<HTMLInputElement>;
       }): ReactElement => {
         updateTotals(selectedRows);
         return (
-          <TableContainer {...getTableContainerProps()}>
+          <TableContainer
+            title="Choose your ingredients"
+            {...getTableContainerProps()}
+          >
+            <TableToolbar>
+              <TableToolbarContent>
+                {/* pass in `onInputChange` change here to make filtering work */}
+                <TableToolbarSearch
+                  persistent={true}
+                  onChange={onInputChange}
+                  placeholder="Search ingredients"
+                />
+                <Button onClick={() => addTacoHandler()}>Add Taco</Button>
+              </TableToolbarContent>
+            </TableToolbar>
             <Table {...getTableProps()}>
               <TableHead>
                 <TableRow>
                   <TableSelectAll {...getSelectionProps()} />
-                  {headers.map((header: any, i: any) => (
+                  {headers.map((header: Headers, i: number) => (
                     <TableHeader key={i} {...getHeaderProps({ header })}>
                       {header.header}
                     </TableHeader>
@@ -57,14 +81,14 @@ const DataTableComponent = ({
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows.map((row: any) => (
+                {rows.map((row: Rows) => (
                   <TableRow key={row.id} {...getRowProps({ row })}>
                     <TableSelectRow
                       {...getSelectionProps({
                         row,
                       })}
                     />
-                    {row.cells.map((cell: any) => (
+                    {row.cells.map((cell: Cells) => (
                       <TableCell key={cell.id}>{cell.value}</TableCell>
                     ))}
                   </TableRow>
